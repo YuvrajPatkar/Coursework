755acf4a96029ae457a27b88ca8b07820de0877e
Yuvraj Patkar <patkar.y@etoncollege.org.uk>
2024-09-06 14:32:39 +0100

    Initial Commit

---
+++ Tradle/TradleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradleApp.swift
+//  Tradle
+//
+//  Created by Patkar, Yuvraj (NA) on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TradleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
